4784d4f4bce728d04b02700e0a594e5be7cbbc71
Charith Hansika <143210650+CharithHansika@users.noreply.github.com>
2026-05-14 11:16:09 +0530

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,1 +1,12 @@
+export const metadata = {
+  title: 'Gemini Chatbot',
+  description: 'Simple AI Chatbot using Gemini Flash',
+}
 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="si">
+      <body>{children}</body>
+    </html>
+  )
+}
